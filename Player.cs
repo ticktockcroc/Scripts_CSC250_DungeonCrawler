12825b3849b76d6b36e6fe6a4536a357c684549b
ticktockcroc <pickartcj@gmail.com>
2024-02-11 14:37:15 -0600

    Edited Player

--- Player.cs
+++ Player.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class Player : MonoBehaviour
+public class Player
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private string name;
+    private int hp;
 
-    // Update is called once per frame
-    void Update()
+    public Player(string name)
     {
-        
+        this.hp = (int)Random.Range(10.0f, 20.0f); //Unity random number generator
+        this.name = name;
+    }
+    public string getName()
+    {
+        return this.name;
+    }
+    public int getHP()
+    {
+        return this.hp;
+    }
+    public void display()
+    {
+        Debug.Log(this.name + "-> HP: " + this.hp);
     }
 }
--- Player1Controller.cs
+++ Player1Controller.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class Player1Controller : MonoBehaviour
+public class Player1Controller : MonoBehaviour //operations below are all performed due to being inherited from class MonoBehavior
 {
-    // Start is called before the first frame update
+    private Player thePlayer;
+    public float speed = 5.0f;
+
+
+    public TextMeshProUGUI playerName;
+    public TextMeshProUGUI playerHp;
+    public GameObject destinationGO;
+
     void Start()
     {
-        
+        this.thePlayer = new Player("John");
+        SetCountText();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        this.thePlayer.display();
+        if (Vector3.Distance(this.gameObject.transform.position, this.destinationGO.transform.position) > 2.0f)
+        {
+            this.gameObject.transform.position = Vector3.Slerp(this.gameObject.transform.position, this.destinationGO.transform.position, this.speed * Time.deltaTime);
+        }
+    }
+
+    void SetCountText()
+    {
+        this.playerName.text = thePlayer.getName();
+        this.playerHp.text = "HP : " + thePlayer.getHP().ToString();
     }
 }
